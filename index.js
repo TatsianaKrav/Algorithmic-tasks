583b5a491f520079a8c11047d37a07ef96135a61
Tatiana <tats.krauchanka@gmail.com>
2025-03-26 00:13:53 +0300

    feat: foo task about context

--- index.js
+++ index.js
@@ -734,3 +734,36 @@ console.log(`Name: ${object}`); 		// Name: Obj-name
 console.log(+object);            		// 0
 console.log(object + 10);        		// 10 */
 
+
+//
+function foo() {
+    const x = 10;
+    return {
+        x: 20,
+        bar: () => {
+            console.log(this.x)
+        },
+        baz: function () {
+            console.log(this.x)
+        }
+    }
+}
+
+const obj1 = foo();
+console.log('first:')
+obj1.bar(); // window/global.x
+obj1.baz(); //  20
+
+const obj2 = foo.call({ x: 30 });
+let y = obj2.bar;
+let z = obj2.baz;
+
+console.log('second:')
+y(); // 30
+z(); //  window/global.x
+
+console.log('third:')
+obj2.bar(); // 30 
+obj2.baz(); // 20 
+
+
